c98e449af43a538ef240ca70241f3c8717346d4a
Stobsoft <130674527+stobsoft@users.noreply.github.com>
2023-04-19 23:29:00 +0800

    Update oljtelegram.sh

--- dev/oljtelegram.sh
+++ dev/oljtelegram.sh
@@ -2,5 +2,5 @@ curl -s $OLJ_JOB_URL | grep -oP $OLJ_JOB_URL_PATTERN > joburl.txt && md5sum jobu
 if cmp --silent -- "currenthashed.txt" "hashed_jobs/hashed.txt"; then
   echo "Files contents are identical. Ignoring..."
 else
-  echo "Updating contents" && rm "hashed_jobs/hashed.txt" && cp currenthashed.txt "hashed_jobs/hashed.txt" && CURRENT_URL=$(head -n1 joburl.txt) && echo $CURRENT_URL && curl -X POST $TELEGRAM_API -d "${TELEGRAM_MESSAGE} ${CURRENT_URL}"
+  echo "Updating contents" && rm "hashed_jobs/hashed.txt" && cp currenthashed.txt "hashed_jobs/hashed.txt" && CURRENT_URL=$(head -n1 joburl.txt) && echo $CURRENT_URL && curl -X POST $TELEGRAM_API -d "${TELEGRAM_MESSAGE} ${CURRENT_URL}" && git add hashed_jobs && git commit -m "Updating hashed" && git push
 fi
