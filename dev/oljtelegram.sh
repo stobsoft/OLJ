708e2c2aa16280e5e97ec2bdc61a8007495277fd
Stobsoft <130674527+stobsoft@users.noreply.github.com>
2023-04-19 19:49:44 +0800

    Create oljtelegram.sh

---
+++ dev/oljtelegram.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+curl -s $OLJ_JOB_URL | grep -oP $OLJ_JOB_URL_PATTERN > joburl.txt && md5sum joburl.txt > "currenthashed.txt" && \
+if cmp --silent -- "currenthashed.txt" "hashed_jobs/hashed.txt"; then
+  echo "Files contents are identical. Ignoring..."
+else
+  echo "Updating contents" && rm "hashed_jobs/hashed.txt" && cp currenthashed.txt "hashed_jobs/hashed.txt" && CURRENT_URL=$(head -n1 joburl.txt) && echo $CURRENT_URL && curl -X POST $TELEGRAM_API -d "${TELEGRAM_MESSAGE} ${CURRENT_URL}"
+fi
